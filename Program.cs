8bb1eebe7e865d525a97a8f61db8efefa09f2ca2
verenstechworld <kenolyoveren@gmail.com>
2024-10-19 00:44:31 +0200

    hellovtw

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace VTWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your Name: ");
+            string name = (Console.Readline ();
+            Console.WriteLine("Hello " + name);
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
